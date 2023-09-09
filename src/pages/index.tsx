cf40d14c0a3fb44d992bdaf8217bc373cf0c6557
PauloHSOliveira <paulooliveiradev@gmail.com>
2023-09-08 21:59:02 -0300

    add supabase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,22 @@ import {
   Tabs,
 } from '@chakra-ui/react';
 import Category from '@/components/Category/Category';
+import { supabase } from '@/service/supabase';
+import { useCallback, useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const getData = useCallback(async () => {
+    const { data, count } = await supabase.from('countries').select();
+    // eslint-disable-next-line no-console
+    console.log(data, count);
+  }, []);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <Container className={inter.className} width="100%">
       <Flex direction="column" width="100%">
